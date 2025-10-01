637a0381df99999bfa7ef067892433dad64012b2
soojin <jinisook@gmail.com>
2025-10-01 18:16:59 +0900

    08 상속

---
+++ ch2_class/src/inheritance/AccountEx.java
@@ -0,0 +1,27 @@
+package inheritance;
+
+// public class AccountEx {
+// public static void main(String[] args) {
+// CheckingAccount checkingAccount = new CheckingAccount("111", "성이름", 1000000,
+// "111-12");
+
+// // 입금
+// checkingAccount.deposit(200000);
+// // 입금 후 잔액
+// System.out.println("잔액 : " + checkingAccount.getBalance());
+// // 직불카드 사용
+// try {
+// System.out.println("체크카드 지불 후 잔액 : " + checkingAccount.pay("111-12",
+// 350000));
+// } catch (Exception e) {
+// e.printStackTrace();
+// }
+
+// // CreditLineAccount craditLineAccount = new CreditLineAccount("121", "이름성",
+// // 100000, 0);
+// // creditLineAccount.withdraw(5000000);
+
+// // System.out.println("마이너스 금액 : ");
+// }
+
+// }
---
+++ ch2_class/src/inheritance/CreditLineAccount.java
@@ -0,0 +1,20 @@
+package inheritance;
+
+// public class CreditLineAccount extends Account {
+// // 마이너스 한도 : 100000(예금액) + 5000000
+// private int minus;
+
+// public CreditLineAccount(String accontNo, String owner, long balance, int
+// minus) {
+// super(accontNo, owner, balance);
+// this.minus = minus;
+// }
+
+// // 출금한다(마이너스 한도 포함)
+// void withdraw(long amount) throws Exception {
+
+// super.withdraw(amount);
+
+// }
+
+// }
